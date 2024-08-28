de3c942e880c3fb2ce7d80287d24f41564a5d084
rohankhan5655 <133367913+rohankhan5655@users.noreply.github.com>
2024-08-28 02:11:51 -0700

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/advice-generator-app/', // Replace with your GitHub repository name
+  base: '/Advice-Generator-Api/', // Replace with your GitHub repository name
 
 })
